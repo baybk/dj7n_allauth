a0f07ed6a907183309f7daca6f0d693151b52122
BayNguyen <baybknguyen@gmail.com>
2025-07-29 22:17:26 +0700

    ok

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     install_requires=[
         "django>=4.2,<5.0",
         "django-allauth>=65.10.0",
-        "requests>=2.32"
-        "jwt>=1.4",
+        "requests>=2.32.4",
+        "jwt>=1.4.0",
         "PyJWT>=2.9.0",
         "djangorestframework-simplejwt>=5.5.0"
     ],
